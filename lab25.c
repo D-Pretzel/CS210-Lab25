fddb011d22164d5f92bcf9ca151f6dace5071d4a
David Petzold <petzoldavid02@gmail.com>
2022-10-23 22:51:47 -0600

    Why is an int* 8 bytes and an int 4...

--- lab25.c
+++ lab25.c
@@ -6,23 +6,24 @@
 int main() {
 
     // Task 1:  Gets the Number of Rows and Columns
+    int numRows = 0;
+    int playerRow = 0;
+    int numCols = 0;
+    int playerCol = 0;
 
-
-    // Task 1 (Continued):  Get the Player's Starting Row and Column
-
+    scanf("%d %d %d %d", &numRows, &numCols, &playerRow, &playerCol);
 
     // Task 2:  Create a Dynamically Sized Map and Place the Character In It
-
+    int** map = createMap(numRows, numCols, playerRow, playerCol);
 
     // This is used to store user commands
     char command[50] = "";
 
     // This keeps animating our "game" until the user types exit
     while (strcmp(command, "exit") != 0) {
-        
         // Task 3:  Print the Current Map
         // Call the function once you have implemented it in lab25functs.c
-        
+        printMap(map, numRows, numCols);
 
         // Asks the user for the next command
         printf("Enter Command ('left', 'right', 'up', 'down', 'exit'): ");
@@ -30,7 +31,15 @@ int main() {
 
         // Task 4:  Calls the appropriate move function
         // e.g., if the user types 'left', call moveLeft
-
+        if (strcmp(command, "left") == 0){
+            moveLeft(map, numRows, numCols, &playerRow, &playerCol);
+        } else if (strcmp(command, "right") == 0){
+            moveRight(map, numRows, numCols, &playerRow, &playerCol);
+        } else if (strcmp(command, "up") == 0){
+            moveUp(map, numRows, numCols, &playerRow, &playerCol);
+        } else if (strcmp(command, "down") == 0){
+            moveDown(map, numRows, numCols, &playerRow, &playerCol);
+        }
 
         // This just adds a space between map prints
         printf("\n");
@@ -38,8 +47,9 @@ int main() {
     }
 
     // Task 6:  Frees the Dynamically Sized Map
-
+    for (int i = 0; i < numRows; i++) {
+        free(map[i]);
+    }
 
     return 0;
-
 }
--- lab25functs.c
+++ lab25functs.c
@@ -12,30 +12,71 @@
 #include "lab25functs.h"
 
 int** createMap(int numRows, int numCols, int playerRow, int playerCol) {
-    
-    return NULL;
+    // Task 2:  Create a Dynamically Sized Map and Place the Character In It
+    int** map = malloc(numRows * sizeof(int*));
+    for (int i = 0; i < numRows; i++) {
+        map[i] = malloc(numCols * sizeof(int));
+    }
+
+    //Set all values to 0
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numCols; j++) {
+            map[i][j] = 0;
+        }
+    }
+
+    map[playerRow][playerCol] = 1;
+    return map;
 }
 
 void printMap(int** map, int numRows, int numCols) {
-    printf("TODO:  Display the map.\n");
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numCols; j++) {
+            printf("%d ", map[i][j]);   // Prints the map
+        }
+        printf("\n");
+    }
 }
 
 void moveLeft(int** map, int numRows, int numCols, int* playerRow, int* playerCol) {
-    
+    if ((map[*playerRow][*playerCol] == PLAYER) && (*playerCol > 0)) {
+        map[*playerRow][*playerCol] = CLEAR;
+        map[*playerRow][*playerCol - 1] = PLAYER;
+        *playerCol = *playerCol - 1;    // Updates the player's column
+        printf("player column is now: %d\n", *playerCol);
+    }
 }
 
 void moveRight(int** map, int numRows, int numCols, int* playerRow, int* playerCol) {
-   
+    if (map[*playerRow][*playerCol + 1] == CLEAR) {
+        map[*playerRow][*playerCol] = CLEAR;     // Sets the current position to 0
+        map[*playerRow][*playerCol + 1] = PLAYER;    // Sets the new position to 1
+        *playerCol = *playerCol + 1;    // Updates the player's column
+        printf("player column is now: %d\n", *playerCol);
+    }
 }
 
 void moveUp(int** map, int numRows, int numCols, int* playerRow, int* playerCol) {
-    
+    if (map[*playerRow - 1][*playerCol] == CLEAR) {
+        map[*playerRow][*playerCol] = CLEAR;     // Sets the current position to 0
+        map[*playerRow - 1][*playerCol] = PLAYER;    // Sets the new position to 1
+        *playerRow = *playerRow - 1;    // Updates the player's row
+        printf("player row is now: %d\n", *playerRow);
+    }
 }
 
 void moveDown(int** map, int numRows, int numCols, int* playerRow, int* playerCol) {
-    
+    if (map[*playerRow + 1][*playerCol] == CLEAR) {
+        map[*playerRow][*playerCol] = CLEAR;     // Sets the current position to 0
+        map[*playerRow + 1][*playerCol] = PLAYER;    // Sets the new position to 1
+        *playerRow = *playerRow + 1;    // Updates the player's row
+        printf("player row is now: %d\n", *playerRow);
+    }
 }
 
 void freeMap(int** map, int numRows) {
-    
+    for (int i = 0; i < numRows; i++){
+        free(map[i]);
+    }
+    free(map);
 }
